d8feb123e71a2eab658eada4aa04eda4890eca53
MelisaRivas <rivasmelisa2024@gmail.com>
2023-07-23 22:38:12 -0600

    terminando pf

---
+++ js/fitness.js
@@ -0,0 +1,61 @@
+//Creando class de nutricion para una seccion de comida saludable con sus detalles
+class Comida{
+    constructor(id, nombre, calorias, categoria, proteinas, grasas){
+        this.id = id
+        this.nombre =  nombre,
+        this.categoria = categoria,
+        this.calorias = calorias,
+        this.proteinas = proteinas,
+        this.grasas = grasas
+    }
+}
+
+//variables
+let comida = document.querySelector("#comidas")
+let h6Cmd = document.querySelector("#h6Cmd")
+let spinnerCmd = document.querySelector("#spinnerCmd")
+
+//Creamos array para poder meter las comidas a las class para luego imprimirlas en el dom
+const comidas = []
+const menuComidas = async () => {
+    const cmd = await fetch("../comidas.json")
+    const detalle = await cmd.json()
+
+    for(let comida of detalle ){
+        let comidaData = new Comida(comida.id, comida.nombre, comida.calorias, comida.categoria, comida.proteinas, comida.grasas)
+        comidas.push(comidaData)
+    }
+    localStorage.setItem("comidas", JSON.stringify(comidas))
+}
+
+function verComidas(array){
+    comida.innerHTML = ""
+    // Empezamos para poder mostrar cards con las comidas
+    for( let data of array){
+        dataDiv = document.createElement("div")
+        dataDiv.className = "col-12 col-xl-4"
+        dataDiv.innerHTML = 
+        `
+        <div id= "id${data.id}" class="card m-3 ">
+            <div class="card-body">
+                <h4 class="card-title" style = "text-align: center"> ${data.id}. ${data.nombre} </h4>
+                <p class = "mt-0" >Categoria: ${data.categoria}g</p>
+                <p class = "mt-0" >Calorias: ${data.calorias}kcal</p>
+                <p class = "mt-0" >Proteina: ${data.proteinas}g</p>
+                <p class = "mt-0" >Grasas: ${data.grasas}g</p>
+                <p class = "text-muted">Cada dato presentado es por una porción de 100g. </p>
+            </div>
+        </div>
+        `
+        comida.appendChild(dataDiv)
+    }
+}
+
+//Metemos las comidas al local storage
+localStorage.getItem("comidas") ? (registro = JSON.parse(localStorage.getItem('comidas'))) : (menuComidas())
+
+setTimeout(() => {
+    spinnerCmd.remove()
+    h6Cmd.remove()
+    verComidas(comidas)
+}, 2500)
--- js/registro.js
+++ js/registro.js
@@ -23,9 +23,33 @@ class Registro{
 }
 
 //Functions
+//Agregando y capturando los valores de los inputs de form
+function agregarDato(registro){
+    let pesoI = document.getElementById("pesoI")
+    let alturaI = document.getElementById("alturaI")
+    let fechaI = document.getElementById("fechaI")
+
+    const nuevoDato = new Registro(registro.length + 1, parseFloat(pesoI.value), parseFloat(alturaI.value), fechaI.value)
+    registro.push(nuevoDato)
+    localStorage.setItem("registro", JSON.stringify(registro))
+    mostrarRegistro(registro)
+    
+    pesoI.value = ""
+    alturaI.value = ""
+    fechaI.value = ""    
+}
+//Sweet alert de success
+function SWalert(){Swal.fire({
+    title: `Tus datos se han agregado exitosamente.`,
+    icon: `success`,
+    confirmButtonText: `Cerrar`,
+    timer: 2800,
+})}
+
 //Boton con la funcion para ver registro
 function mostrarRegistro(array){
     datosI.innerHTML = ""
+    //modificando el dom y agregando los datos, segun el rango del imc sera el tipo de card que se presentara. Si esta es muy baja o muy alta, se mostrara el texto en rojo, de lo contrario se mostrara verde.
     for(dato of array){
         nuevoDatoDiv = document.createElement("div")
         nuevoDatoDiv.className = "col-12 col-xl-4"
@@ -36,7 +60,7 @@ function mostrarRegistro(array){
                 <h4 class="card-title" style = "text-align: center"> Este dato fue ingresado el ${dato.fechaI}.</h4>
                 <p>Peso registrado: ${dato.pesoI}kg</p>
                 <p>Altura registrado: ${dato.alturaI}m</p>
-                <p class="${dato.imc} malPeso">Tu IMC es: ${dato.imc.toFixed(2)}. Eso es bajo el peso normal.</p>
+                <p class="${dato.imc} malPeso">Tu IMC es: ${Math.round(dato.imc)}. Eso es bajo el peso normal.</p>
                 <button id="btnEliminar${dato.id}" class="btn btn-el">Eliminar</button>
             </div>
         </div>`}
@@ -75,10 +99,13 @@ function mostrarRegistro(array){
         }
         datosI.appendChild(nuevoDatoDiv)
         
-        // agregarDatoBtn.addEventListener("click", () => {
-        //     agregarDato(dato)
-        // })
+        agregarDatoBtn.addEventListener("click", () => {
+            agregarDato(dato)
+            registro.push(agregarDato)
+            localStorage.getItem("registro")
+        })
     }    
+    //Manera para poder eliminar un dato
     array.forEach((dato) => {
         let btnEliminar = document.querySelector(`#btnEliminar${dato.id}`)
         btnEliminar.addEventListener("click", ()=>{
@@ -90,27 +117,23 @@ function mostrarRegistro(array){
             array.splice(posicion,1)
             if (dato.imc !== null) {
                 localStorage.setItem("registro", JSON.stringify(array));
-              }
+            }
+            //alerta de que se elimino
+            Toastify(
+            {
+                text: `Se eliminó el dato`,
+                duration: 3000,
+                gravity: "top",
+                position: "right",
+                style: {
+                    color: "white",
+                    background: "green"
+                }
+            }
+            ).showToast()          
         })
     })
 }
-
-//Agregando y capturando los valores de los inputs de form
-function agregarDato(registro){
-    let pesoI = document.getElementById("pesoI")
-    let alturaI = document.getElementById("alturaI")
-    let fechaI = document.getElementById("fechaI")
-
-    const nuevoDato = new Registro(registro.length + 1, parseFloat(pesoI.value), parseFloat(alturaI.value), fechaI.value)
-    registro.push(nuevoDato)
-    localStorage.setItem("registro", JSON.stringify(registro))
-    
-    pesoI.value = ""
-    alturaI.value = ""
-    fechaI.value = ""
-}
-
-//Funcion para poder eliminar dato
 
 //Agregando funcion para poder ordenar
 function ordenarMenorMayor(array){
@@ -125,33 +148,46 @@ function ordenarMayorMenor(array){
     mostrarRegistro(mayorMenor)
 }
 
+
+//SET TIMEOUT para poder cargar el registro cada que se entra al sitio
+setTimeout(()=>{
+    mostrarRegistro(registro)
+},2000)
+
+
 //EVENTOS:
 agregarDatoBtn.addEventListener("click", (event) => {
     event.preventDefault()
     agregarDato(registro) 
-})
-
-verReigstro.addEventListener("click", () => {
-    mostrarRegistro(registro)
-})
-
-//Boton con la funcion de ocultar registro
-ocultarReigstro.addEventListener("click", () => {
-    //reiniciando el div
-    datosI.innerHTML = ""
+    SWalert()
+    setInterval(()=>{
+        mostrarRegistro(registro)
+    }, 2000)
 })
 
 selectOrden.addEventListener("change", () => {
     switch(selectOrden.value){
-       case "1":
-          ordenarMayorMenor(registro)
-       break
-       case "2":
-          ordenarMenorMayor(registro)
-       break
-       default:
-          mostrarRegistro(registro)
-       break
+        case "1":
+            ordenarMayorMenor(registro)
+        break
+        case "2":
+            ordenarMenorMayor(registro)
+        break
+        default:
+            mostrarRegistro(registro)                
+        break
     }
 })
+            
+//Ocultandola para poder ocupar el Set timeout, se dejara para propositos de referncia de que se podia hacer antes
+// verReigstro.addEventListener("click", () => {
+//     mostrarRegistro(registro)
+// })
+
+// //Boton con la funcion de ocultar registro
+// ocultarReigstro.addEventListener("click", () => {
+//     //reiniciando el div
+//     datosI.innerHTML = ""
+// })
+            
  
